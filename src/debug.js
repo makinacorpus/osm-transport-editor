4d7a873520f1f2bd722291c631839a1f9d6f5736
JeanMichel FRANCOIS <toutpt@gmail.com>
2014-07-14 16:24:48 +0200

    feature:add debug panel

---
+++ src/debug.js
@@ -0,0 +1,11 @@
+/*jshint strict:false */
+/*global angular:false */
+
+angular.module('osm.controllers').controller('DebugController',
+	['$scope', function($scope){
+		$scope.displayDebugPanel = false;
+		$scope.toggleDebugPanel = function(){
+			$scope.displayDebugPanel = !$scope.displayDebugPanel;
+		}
+	}]
+);
